b38e8484ae79fcc0e24e44fdefa137d2dfd2e9cd
farther <farther.mail@gmail.com>
2019-11-08 13:36:14 +0200

    added  Consolidated param

--- lib/paysafe/account_management_service.rb
+++ lib/paysafe/account_management_service.rb
@@ -59,7 +59,8 @@ module Paysafe
     def getMerchantAccountInfo merchantAccount
       request = Request.new(
         method: Request::GET,
-        uri: prepare_uri("/accounts/#{merchantAccount.merchantId}")
+        uri: prepare_uri("/accounts/#{merchantAccount.merchantId}"),
+        query: { operationMode: 'consolidated' }
       )
 
       response = @client.process_request request
