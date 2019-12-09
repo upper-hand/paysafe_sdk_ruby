2ab833df1d6f73b514b7dddba20ac3ab2f7f0178
MT <mtrilus@gmail.com>
2019-12-09 11:14:00 -0500

    feat(json): add snake case method

--- lib/paysafe/json_object.rb
+++ lib/paysafe/json_object.rb
@@ -12,12 +12,29 @@ module Paysafe
       vars = {}
 
       instance_variables.map do |name|
-        s    = name.to_s
+        s = name.to_s
         s[0] = ''
 
         if !ignore.include?(s) && (!instance_variable_get(name).nil? || required.include?(s))
           vars[s] = instance_variable_get(name)
         end
+      end
+
+      vars
+    end
+
+    def to_snake_case
+      vars = {}
+
+      instance_variables.map do |name|
+        name.to_s
+          .gsub(/::/, '/')
+          .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+          .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+          .tr('-', '_')
+          .downcase
+
+        vars[s] = instance_variable_get(name)
       end
 
       vars
