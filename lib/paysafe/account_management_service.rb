33e7642f640c28fb66a65d5e00a5708b4844d77e
Michael Babin <iamsuperman@ukr.net>
2021-11-11 02:37:17 -0500

    fix comma

--- lib/paysafe/account_management_service.rb
+++ lib/paysafe/account_management_service.rb
@@ -83,7 +83,7 @@ module Paysafe
             'dateOfBirth',
             'ssn',
             'isApplicant',
-            'isControlProng',
+            'isControlProng'
           ],
           ignore = ['accountId']
         )
