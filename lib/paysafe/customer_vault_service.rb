d98495b9d6a934d26fd3a446834bce650d367088
Misha Babin <michaelbabin.eu@gmail.com>
2024-06-07 09:57:33 +0300

    added single use token for ACH

--- lib/paysafe/customer_vault_service.rb
+++ lib/paysafe/customer_vault_service.rb
@@ -1,15 +1,15 @@
 <<-DOC
  * Copyright (c) 2016 Paysafe
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -506,6 +506,16 @@ module Paysafe
     ####################
     # ACH Bank Account
     ####################
+
+    def create_ach_singleUseToken body
+      request = Request.new(
+        method: Request::POST,
+        uri: prepare_uri('/achsingleusetokens'),
+        body: body
+      )
+
+      @client.process_request request
+    end
 
     def create_ach_bank_account ach_bank_account
       request = Request.new(
